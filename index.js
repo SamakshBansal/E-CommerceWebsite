e2964c9d588f9dcf227a15f208219fed7fdcbe94
Samaksh Bansal <samaksh199@gmail.com>
2022-03-23 17:08:59 +0530

    blog part

--- index.js
+++ index.js
@@ -50,4 +50,21 @@ $(document).ready(function () {
       },
     },
   });
+
+// blogs owl carousel
+    $("#blogs .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive : {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            }
+        }
+    })
+
+
 });
