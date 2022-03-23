a8d47618ea85c24af568e032eff0b2e706c6aca1
Samaksh Bansal <samaksh199@gmail.com>
2022-03-23 17:12:43 +0530

    product added

--- index.js
+++ index.js
@@ -51,20 +51,41 @@ $(document).ready(function () {
     },
   });
 
-// blogs owl carousel
-    $("#blogs .owl-carousel").owlCarousel({
-        loop: true,
-        nav: false,
-        dots: true,
-        responsive : {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 3
-            }
-        }
-    })
+  // blogs owl carousel
+  $("#blogs .owl-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+    },
+  });
 
+  // product qty section
+  let $qty_up = $(".qty .qty-up");
+  let $qty_down = $(".qty .qty-down");
+  let $input = $(".qty .qty_input");
 
+  // click on qty up button
+  $qty_up.click(function (e) {
+    if ($input.val() >= 1 && $input.val() <= 9) {
+      $input.val(function (i, oldval) {
+        return ++oldval;
+      });
+    }
+  });
+
+  // click on qty down button
+  $qty_down.click(function (e) {
+    if ($input.val() > 1 && $input.val() <= 10) {
+      $input.val(function (i, oldval) {
+        return --oldval;
+      });
+    }
+  });
 });
