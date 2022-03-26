668709a308f2ee1218c87e9119fc4b3d667637d7
Samaksh Bansal <samaksh199@gmail.com>
2022-03-26 16:29:17 +0530

    quantity change

--- Template/_cart-template.php
+++ Template/_cart-template.php
@@ -44,12 +44,13 @@
                         <!-- product qty -->
                         <div class="qty d-flex pt-2">
                             <div class="d-flex font-rale w-25">
-                                <button class="qty-up border bg-light" data-id="pro1"><i
+                                <button class="qty-up border bg-light"
+                                    data-id="<?php echo $item['item_id'] ?? '0'; ?>"><i
                                         class="fas fa-angle-up"></i></button>
-                                <input type="text" data-id="pro1" class="qty_input border px-2 w-100 bg-light" disabled
-                                    value="1" placeholder="1">
-                                <button data-id="pro1" class="qty-down border bg-light"><i
-                                        class="fas fa-angle-down"></i></button>
+                                <input type="text" data-id="<?php echo $item['item_id'] ?? '0'; ?>"
+                                    class="qty_input border px-2 w-100 bg-light" disabled value="1" placeholder="1">
+                                <button data-id="<?php echo $item['item_id'] ?? '0'; ?>"
+                                    class="qty-down border bg-light"><i class="fas fa-angle-down"></i></button>
                             </div>
 
                             <form method="post">
@@ -66,7 +67,8 @@
 
                     <div class="col-sm-2 text-right">
                         <div class="font-size-20 text-danger font-baloo">
-                            $<span class="product_price"><?php echo $item['item_price'] ?? 0; ?></span>
+                            $<span class="product_price"
+                                data-id="<?php echo $item['item_id'] ?? '0'; ?>"><?php echo $item['item_price'] ?? 0; ?></span>
                         </div>
                     </div>
                 </div>
@@ -83,8 +85,9 @@
                     <h6 class="font-size-12 font-rale text-success py-3"><i class="fas fa-check"></i> Your order is
                         eligible for FREE Delivery.</h6>
                     <div class="border-top py-4">
-                        <h5 class="font-baloo font-size-20">Subtotal ( <?php echo count($subTotal) ?? 0; ?> item):&nbsp;
-                            <span class="text-danger">$<span class="text-danger"
+                        <h5 class="font-baloo font-size-20">Subtotal (
+                            <?php echo isset($subTotal) ? count($subTotal) : 0; ?> item):&nbsp; <span
+                                class="text-danger">$<span class="text-danger"
                                     id="deal-price"><?php echo isset($subTotal) ? $Cart->getSum($subTotal) : 0; ?></span>
                             </span> </h5>
                         <button type="submit" class="btn btn-warning mt-3">Proceed to Buy</button>
